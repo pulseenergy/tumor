57bd4c7499cc19a148dea60800df43d1f2095b9a
Neil Gentleman <ngentleman@gmail.com>
2015-01-29 16:55:42 -0800

    use npm-package-arg to parse versions

--- test/tumorTest.js
+++ test/tumorTest.js
@@ -3,22 +3,6 @@ var assert = require('assert'),
 
 describe('tumor', function () {
 
-	describe('expectedPath', function () {
-
-		it('works for git:// urls', function () {
-			assert.equal(tumor.expectedPath('git://github.com/pulseenergy/tumor.git'), 'tumor');
-		});
-
-		it('works for git:// urls with hash', function () {
-			assert.equal(tumor.expectedPath('git://github.com/pulseenergy/tumor.git#master'), 'tumor');
-		});
-
-		it('works for git+ssh:// urls with hash', function () {
-			assert.equal(tumor.expectedPath('git+ssh://git@github.com:pulseenergy/tumor.git#master'), 'tumor');
-		});
-
-	});
-
 	describe('matchersFromRemote', function () {
 
 		var examples = {
